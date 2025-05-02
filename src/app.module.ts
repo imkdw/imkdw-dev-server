89443e20442876654ca0373eb6d919e4cb8a1a63
dongwoo <imkdw@kakao.com>
2025-05-02 15:34:51 +0900

    feat: `RoleGuard` 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AllExceptionFilter } from './common/filter/all-exception.filter';
 import { DomainExceptionFilter } from './common/filter/domain-exception.filter';
@@ -7,9 +7,12 @@ import { TransformInterceptor } from './common/interceptor/transform.interceptor
 import { MemoModule } from './modules/memo/memo.module';
 import { ClsPrismaModule } from 'src/config/cls-prisma.module';
 import { AuthModule } from '@/core/auth/auth.module';
+import { JwtGuard } from '@/common/guards/jwt.guard';
+import { MyConfigModule } from '@/core/config/my-config.module';
+import { JwtModule } from '@/infra/jwt/jwt.module';
 
 @Module({
-  imports: [MemoModule, ClsPrismaModule, AuthModule],
+  imports: [MemoModule, ClsPrismaModule, AuthModule, MyConfigModule, JwtModule],
   controllers: [AppController],
   providers: [
     {
@@ -23,6 +26,10 @@ import { AuthModule } from '@/core/auth/auth.module';
     {
       provide: APP_INTERCEPTOR,
       useClass: TransformInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
     },
   ],
 })
