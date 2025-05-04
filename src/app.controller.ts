edaa97310b42697dda6e928327bc536ec04a5114
dongwoo <imkdw@kakao.com>
2025-05-04 20:36:24 +0900

    feat: 헬스체크용 엔드포인트 퍼블릭 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
+import { Public } from '@/common/decorator/public.decorator';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
+  @Public()
   getHello(): string {
     return 'Hello World';
   }
